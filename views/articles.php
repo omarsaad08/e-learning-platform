9f44f1dbb49aa3ddf8e34f69d865679cf1fad6de
yasmeen waleed <yasmynwlydf@gmail.com>
2025-04-15 08:49:38 +0200

    articles.php

--- views/articles.php
+++ views/articles.php
@@ -177,6 +177,38 @@
   
   </div>
   <!-- END: Articles Page -->
+  <footer class="footer">
+    <div class="footer-container">
+      <div class="footer-about">
+        <h3>MyCourses</h3>
+        <p>An interactive platform to learn programming from beginner to advanced levels.</p>
+      </div>
+  
+      <div class="footer-links">
+        <h4>Quick Links</h4>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Courses</a></li>
+          <li><a href="#">Articles</a></li>
+          <li><a href="#">About</a></li>
+        </ul>
+      </div>
+  
+      <div class="footer-social">
+        <h4>Follow Us</h4>
+        <div class="social-icons">
+          <a href="#"><img src="facebook_black_logo_icon_147136.png" alt="Facebook" /></a>
+          <a href="#"><img src="twitterlogoincircularblackbutton_79784.png" alt="Twitter" /></a>
+    
+          <a href="#"><img src="linkedin_black_logo_icon_147114.png" alt="linkedin" /></a>
+        </div>
+      </div>
+    </div>
+  
+    <div class="footer-bottom">
+      <p>© 2025 MyCourses. All rights reserved.</p>
+    </div>
+  </footer>
   
  
 <script src="js.js"></script>
