81c10d7402bdb7f3b02784031c7c4f4136e12508
yasmeen waleed <yasmynwlydf@gmail.com>
2025-04-15 08:48:53 +0200

    courses.php

--- views/courses.php
+++ views/courses.php
@@ -6,6 +6,7 @@
     <meta name="description" content="Master programming from beginner to advanced with interactive online courses in Python, JavaScript, web development, data science, and more. Learn at your own pace with real-world projects and expert instructors.">
     <meta name="keywords" content="learn programming, online coding courses, programming for beginners, advanced coding tutorials, Python course, JavaScript training, HTML CSS course, web development bootcamp, data science classes, machine learning tutorials, full stack development, front end development, back end programming, coding interview prep, software engineering course, mobile app development, React course, Node.js training, coding for kids, self-paced coding lessons, interactive coding platform, build real-world projects, computer science fundamentals, AI programming course, online coding certification, remote coding school, code with projects, programming mentorship, tech career training, coding challenges, algorithm tutorials, css, js, html,c++, data structure ,oop, computer ,programming,courses">
 <link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="js.js">
     <title>Document</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -23,8 +24,8 @@
       <h1 class="logo">MyCourses</h1>
       <ul class="nav-links">
         <li><a href="#">Home</a></li>
-        <li><a href="#">Courses</a></li>
-        <li><a href="#">Articles</a></li>
+        <li><a href="index.html">Courses</a></li>
+        <li><a href="article.html">Articles</a></li>
         <li><a href="#">About</a></li>
       </ul>
     </div>
@@ -186,7 +187,41 @@
       </div>
     </main>
   </div>
- 
-<script src="script.js"></script>
+ <!-- END: Articles Page -->
+ <footer class="footer">
+  <div class="footer-container">
+    <div class="footer-about">
+      <h3>MyCourses</h3>
+      <p>An interactive platform to learn programming from beginner to advanced levels.</p>
+    </div>
+
+    <div class="footer-links">
+      <h4>Quick Links</h4>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="index.html">Courses</a></li>
+        <li><a href="article.html">Articles</a></li>
+        <li><a href="#">About</a></li>
+      </ul>
+    </div>
+
+    <div class="footer-social">
+      <h4>Follow Us</h4>
+      <div class="social-icons">
+        <a href="#"><img src="facebook_black_logo_icon_147136.png" alt="Facebook" /></a>
+        <a href="#"><img src="twitterlogoincircularblackbutton_79784.png" alt="Twitter" /></a>
+  
+        <a href="#"><img src="linkedin_black_logo_icon_147114.png" alt="linkedin" /></a>
+      </div>
+    </div>
+  </div>
+
+  <div class="footer-bottom">
+    <p>© 2025 MyCourses. All rights reserved.</p>
+  </div>
+</footer>
+
+
+<script src="js.js"></script>
 </body>
 </html>
